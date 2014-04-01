354c25f2cd9274675d5ca88d4df73d04bcb87da5
Brian Picciano <mediocregopher@gmail.com>
2014-03-31 22:09:32 -0500

    add aliases for logging functions, to make go vet be quiet

--- gslog/levels.go
+++ gslog/levels.go
@@ -31,20 +31,32 @@ func Debug(msg string, params ...interface{}) {
 	log(DEBUG, &msg, params)
 }
 
+// Alias of Debug
+var Debugf = Debug
+
 // Info writes a message to the log with info severity
 func Info(msg string, params ...interface{}) {
 	log(INFO, &msg, params)
 }
+
+// Alias of Info
+var Infof = Info
 
 // Warn writes a message to the log with warn severity
 func Warn(msg string, params ...interface{}) {
 	log(WARN, &msg, params)
 }
 
+// Alias of Warn
+var Warnf = Warn
+
 // Error writes a message to the log with error serverity
 func Error(msg string, params ...interface{}) {
 	log(ERROR, &msg, params)
 }
+
+// Alias of Error
+var Errorf = Error
 
 // Fatal writes a message to the log with fatal severity, flushes any
 // messages waiting to be written, and exits with a non-zero status
@@ -53,3 +65,6 @@ func Fatal(msg string, params ...interface{}) {
 	Flush()
 	os.Exit(1)
 }
+
+// Alias of Fatal
+var Fatalf = Fatal
